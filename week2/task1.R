cc433fa61a71bbb24b0fa741fcb412cac3a3d686
tuanstar90208 <40812858+tuanstar90208@users.noreply.github.com>
2018-07-14 20:48:15 +0800

    change

--- week2/task1.R
+++ week2/task1.R
@@ -1,1 +1,13 @@
 # 第二周任務
+library(ggplot2)
+attitude
+
+ggplot(data = attitude, aes(x = rating)) +
+  geom_bar(fill = "lightgreen", colour = "black")
+
+ggplot(data = attitude, aes(x = rating, y=learning)) +
+  geom_point()
+
+ggplot(attitude, aes(x=rating, y=learning)) +
+  geom_boxplot()
+
